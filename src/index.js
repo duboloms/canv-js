19938c64db0a9dcda1cd6625f0716defa77016c0
dubolom <dubolom7349@mail.ru>
2020-04-23 19:45:07 +0300

    Change index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("Потом сделаю файловую системуe!@");
+class Canv {
+  constructor() {
+    this.version = "0.1 indev";
+  }
+
+  square(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
